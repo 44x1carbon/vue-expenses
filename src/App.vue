1ceec802564be12c2be7779b51ba3665e390806f
44 <44x1carbon@gmail.com>
2021-10-14 19:43:43 +0900

    収入・支出詳細を表示

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
+    <section>
+      <h2>収入・支出詳細</h2>
+      <table>
+        <tr>
+          <th>日付</th>
+          <th>内容</th>
+          <th>金額（円）</th>
+          <th>大項目</th>
+          <th>中項目</th>
+          <th>メモ</th>
+        </tr>
+        <tr v-for="row in incomeExpensesData" :key="row.id">
+          <th>{{ row.date }}</th>
+          <th>{{ row.content }}</th>
+          <th>{{ row.amount }}</th>
+          <th>{{ row.largeCategory }}</th>
+          <th>{{ row.middleCategory }}</th>
+          <th>{{ row.memo }}</th>
+        </tr>
+      </table>
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import IncomeExpensesData from './data.json'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      incomeExpensesData: [...IncomeExpensesData],
+    }
+  },
   }
 }
 </script>
